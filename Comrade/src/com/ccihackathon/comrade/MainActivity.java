d12378abeb7d1a21ca8f8ca84938d782395e09bc
Prachi Ghatwal <prachi250290@gmail.com>
2013-10-25 16:07:10 +0530

    dialog fragment ui

--- Comrade/src/com/ccihackathon/comrade/MainActivity.java
+++ Comrade/src/com/ccihackathon/comrade/MainActivity.java
@@ -67,9 +67,7 @@ public class MainActivity extends Activity implements LocationListener {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
 		setupDatabase();
-
 		setupMap();
 
 		addListenersToMap();
